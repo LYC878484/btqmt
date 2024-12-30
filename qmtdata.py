66c6116355762b8d7e6b77051fad223ba62318c1
wawasa <yuchenliu083@gmail.com>
2024-12-31 00:45:36 +0800

    qmt data

--- qmtdata.py
+++ qmtdata.py
@@ -1,2 +1,14 @@
-from datetime import datetime
+import datetime
+
 import backtrader as bt
+from backtrader.feed import DataBase
+from .qmtstore import qmtstore
+
+class MetaQMTData(DataBase.__class__):
+    def __init__(cls, name, bases, dct):
+        '''Class has already been created ... register'''
+        # Initialize the class
+        super(MetaQMTData, cls).__init__(name, bases, dct)
+
+        # Register with the store
+        qmtstore.QMTStore.DataCls = cls

7c4632472a1be49624b6834d4865448ff4f97178
wawasa <yuchenliu083@gmail.com>
2025-01-08 00:28:01 +0800

    qmtstore

--- qmtstore.py
+++ qmtstore.py
@@ -1,9 +1,14 @@
-import datetime
+import time, datetime
 import collections
 
 import backtrader as bt
 from backtrader.metabase import MetaParams
+from xtquant.xttrader import XtQuantTrader, XtQuantTraderCallback
+from xtquant.xttype import StockAccount
 
+
+class MyXtQuantTraderCallback(XtQuantTraderCallback):
+    pass
 
 class MetaSingleton(MetaParams):
     '''Metaclass to make a metaclassed class a singleton'''
@@ -37,8 +42,29 @@ class QMTStore(metaclass=MetaSingleton):
         self.qs = collections.OrderedDict()  # key: tickerId -> queues
         self.ts = collections.OrderedDict()  # key: queue -> tickerId
 
+
+        # 指定客户端所在路径, 券商端指定到 userdata_mini文件夹
+        # 注意：如果是连接投研端进行交易，文件目录需要指定到f"{安装目录}\userdata"
+        path = r'D:\国金证券QMT交易端\bin.x64/../userdata_mini'
+        # 生成session id 整数类型 同时运行的策略不能重复
+        session_id = int(time.time())
+        self.xt_trader = XtQuantTrader(path, session_id)
+
+        # 创建资金账号为 800068 的证券账号对象 股票账号为STOCK 信用CREDIT 期货FUTURE
+        acc = StockAccount('8886991198', 'STOCK')
+        # 创建交易回调类对象，并声明接收回调
+        callback = MyXtQuantTraderCallback()
+        self.xt_trader.register_callback(callback)
+        # 建立交易连接，返回0表示连接成功
+        connect_result = self.xt_trader.connect()
+        print('建立交易连接, 返回0表示连接成功', connect_result)
+
     def start(self, data=None, broker=None):
-        pass
+        # 启动交易线程
+        self.xt_trader.start()
+        # 对交易回调进行订阅，订阅后可以收到交易主推，返回0表示订阅成功
+        subscribe_result = self.xt_trader.subscribe(acc)
+        print('对交易回调进行订阅, 订阅后可以收到交易主推, 返回0表示订阅成功', subscribe_result)
 
     def stop(self):
         pass

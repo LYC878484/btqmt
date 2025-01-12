396427d2249811b5236e500d2d2067abbd128655
wawasa <yuchenliu083@gmail.com>
2025-01-12 19:09:03 +0800

    获取qmt指定股票的历史数据并且转化为backtrader可识别的结构

--- qmtstore.py
+++ qmtstore.py
@@ -1,4 +1,5 @@
 import time, datetime
+import sys
 import collections
 
 import backtrader as bt
@@ -8,7 +9,79 @@ from xtquant.xttype import StockAccount
 
 
 class MyXtQuantTraderCallback(XtQuantTraderCallback):
-    pass
+    def on_connected(self):
+        """
+        连接成功推送
+        """
+        print(datetime.datetime.now(),'连接成功回调')
+
+    def on_disconnected(self):
+        """
+        连接断开
+        :return:
+        """
+        print(datetime.datetime.now(),'连接断开回调')
+
+    def on_stock_order(self, order):
+        """
+        委托回报推送
+        :param order: XtOrder对象
+        :return:
+        """
+        print(datetime.datetime.now(), '委托回调', order.order_remark)
+
+
+    def on_stock_trade(self, trade):
+        """
+        成交变动推送
+        :param trade: XtTrade对象
+        :return:
+        """
+        print(datetime.datetime.now(), '成交回调', trade.order_remark)
+
+
+    def on_order_error(self, order_error):
+        """
+        委托失败推送
+        :param order_error:XtOrderError 对象
+        :return:
+        """
+        # print("on order_error callback")
+        # print(order_error.order_id, order_error.error_id, order_error.error_msg)
+        print(f"委托报错回调 {order_error.order_remark} {order_error.error_msg}")
+
+    def on_cancel_error(self, cancel_error):
+        """
+        撤单失败推送
+        :param cancel_error: XtCancelError 对象
+        :return:
+        """
+        print(datetime.datetime.now(), sys._getframe().f_code.co_name)
+
+    def on_order_stock_async_response(self, response):
+        """
+        异步下单回报推送
+        :param response: XtOrderResponse 对象
+        :return:
+        """
+        print(f"异步委托回调 {response.order_remark}")
+
+    def on_cancel_order_stock_async_response(self, response):
+        """
+        收到撤单回调信息
+        :param response: XtCancelOrderResponse 对象
+        :return:
+        """
+        print(datetime.datetime.now(), sys._getframe().f_code.co_name)
+
+    def on_account_status(self, status):
+        """
+        账号状态信息变动推送
+        :param response: XtAccountStatus 对象
+        :return:
+        """
+        print(datetime.datetime.now(), sys._getframe().f_code.co_name)
+
 
 class MetaSingleton(MetaParams):
     '''Metaclass to make a metaclassed class a singleton'''
@@ -39,29 +112,35 @@ class QMTStore(metaclass=MetaSingleton):
     def __init__(self):
         super(QMTStore, self).__init__()
         # Structures to hold datas requests
-        self.qs = collections.OrderedDict()  # key: tickerId -> queues
-        self.ts = collections.OrderedDict()  # key: queue -> tickerId
-
         # 指定客户端所在路径, 券商端指定到 userdata_mini文件夹
         # 注意：如果是连接投研端进行交易，文件目录需要指定到f"{安装目录}\userdata"
         path = r'D:\国金证券QMT交易端\bin.x64/../userdata_mini'
+        self.connect(path=path)
+
+        self.qs = collections.OrderedDict()  # key: tickerId -> queues
+        self.ts = collections.OrderedDict()  # key: queue -> tickerId
+
+    def connect(self, path=None):
         # 生成session id 整数类型 同时运行的策略不能重复
         session_id = int(time.time())
         self.xt_trader = XtQuantTrader(path, session_id)
 
-        # 启动交易线程
-        self.xt_trader.start()
-
         # 创建资金账号为 800068 的证券账号对象 股票账号为STOCK 信用CREDIT 期货FUTURE
-        acc = StockAccount('8886991198', 'STOCK')
-        # 建立交易连接，返回0表示连接成功
-        connect_result = self.xt_trader.connect()
-        print('建立交易连接, 返回0表示连接成功', connect_result)
+        account = StockAccount('8886991198', 'STOCK')
+
         # 创建交易回调类对象，并声明接收回调
         callback = MyXtQuantTraderCallback()
         self.xt_trader.register_callback(callback)
+
+        # 启动交易线程
+        self.xt_trader.start()
+
+        # 建立交易连接，返回0表示连接成功
+        connect_result = self.xt_trader.connect()
+        print('建立交易连接(返回0表示连接成功):', connect_result)
+
         # 对交易回调进行订阅，订阅后可以收到交易主推，返回0表示订阅成功
-        subscribe_result = self.xt_trader.subscribe(acc)
+        subscribe_result = self.xt_trader.subscribe(account)
         print('对交易回调进行订阅, 订阅后可以收到交易主推, 返回0表示订阅成功', subscribe_result)
 
     def start(self, data=None, broker=None):
---
+++ run.py
@@ -0,0 +1,30 @@
+import backtrader as bt
+import qmtstore
+import qmtbroker
+import qmtdata
+
+def runstrategy():
+    # Create a cerebro
+    cerebro = bt.Cerebro()
+
+    qmt_store = qmtstore.QMTStore()
+    qmt_broker = qmtbroker.QMTBroker()
+
+
+    #stock = '600000.SH'
+    qmt_data = qmtdata.QMTData()
+    cerebro.setbroker(qmt_broker)
+
+    #hisdata = qmt_data.history_data(stock="600406.SH", period="1d", start="20240101", end="20240401")
+    #print(hisdata)
+
+    seq = qmt_data.subscribe_live_data(stock="600406.SH", period="1m", start="20240101", end="20240401")
+
+    #cerebro.adddata(data0)
+
+
+    cerebro.run()
+
+
+if __name__ == '__main__':
+    runstrategy()
